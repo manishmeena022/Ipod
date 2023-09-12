acfeff4a76ff9e2019fb115489187a0d42d502a0
Manish  meena <nsit2016manish@gmail.com>
2023-09-12 22:59:40 +0530

    first commit

---
+++ src/Artists.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import artistImage from './assets/images/user.webp';
+
+
+class Artists extends React.Component{
+
+    render(){
+        return(
+            <div style={styles.artistsContainer}>
+                <div style={styles.titleBar}>
+                    <p style={{fontWeight: 'bold'}}>IPod</p>
+                        <img style={styles.battery} src="https://cdn-icons-png.flaticon.com/128/664/664883.png" alt="battery" />
+                </div>
+
+                <div style={styles.info}>
+                    <div style={styles.imageContainer}>
+                    </div>
+                    
+                    <div style={styles.subInfo}>
+                        <h4 style={{marginBottom: '0.5rem'}}>IPod.js </h4>
+                        <p style={{marginBottom:'0'}}>React Project</p>
+                        <p>Made by : Manish Meena</p>
+                    </div>
+                </div>
+
+                <div style={styles.info2}>
+                    <h5 style={{alignSelf : 'center'}}>Thanks for visiting the app !!</h5>
+                </div>
+            </div>
+        );
+    }
+}
+
+const styles = {
+    artistsContainer: {
+        height : '100%',
+        width: '100%',
+        display : 'flex',
+        flexDirection : 'column'
+    },
+    imageContainer : {
+        height : '65%',
+        width : '35%',
+        borderRadius : '50%',
+        backgroundImage : `url(${artistImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition : 'center',
+        alignSelf: 'center'
+    },
+    image: {
+        width: '2rem',
+        height: '2rem'
+    },
+    info : {
+        height: '70%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    info2 : {
+        height: '30%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap : 'wrap',
+        justifyContent: 'center'
+    },
+    subInfo: {
+        alignSelf: 'center'
+    },
+    titleBar: {
+        height: '10%',
+        width: '100%',
+        borderRadius: '12px 0 0 0',
+        backgroundImage: 'linear-gradient(0deg, rgb(123,  132, 140),transparent)',
+        borderBottom: '1px solid #6c757d',
+        padding: '1px 5px 10px 8px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    battery: {
+        width : '20px',
+        height: '20px'
+    }
+}
+
+export default Artists;
